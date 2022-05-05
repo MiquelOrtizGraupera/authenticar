751eb472eac59e8f4a661e5194a02dd888166a55
Miquel Ortiz <miquel.ortiz.7e4@itb.cat>
2022-05-05 12:32:31 +0200

    FIRST COMMIT

--- src/main/java/com/example/basicaut/model/repositori/RepositoriUsuari.java
+++ src/main/java/com/example/basicaut/model/repositori/RepositoriUsuari.java
@@ -2,9 +2,11 @@ package com.example.basicaut.model.repositori;
 
 import com.example.basicaut.model.entitats.Usuari;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RepositoriUsuari extends JpaRepository<Usuari, Long> {
     Optional<Usuari> findByUserName(String username);
 }
--- src/main/java/com/example/basicaut/model/serveis/ElMeuUserDetailService.java
+++ src/main/java/com/example/basicaut/model/serveis/ElMeuUserDetailService.java
@@ -1,12 +1,16 @@
 package com.example.basicaut.model.serveis;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class ElMeuUserDetailService implements UserDetailsService {
-    private ServeiUsuari serveiUsuariUserDetails;
 
+    private final ServeiUsuari serveiUsuariUserDetails;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
